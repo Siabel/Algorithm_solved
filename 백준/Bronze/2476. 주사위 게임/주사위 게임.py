6deb6335d45c884b155c63082d18d7f799cddc4e
Siabel <128214383+Siabel@users.noreply.github.com>
2023-09-06 02:32:09 +0900

    [Bronze III] Title: 주사위 게임, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -0,0 +1,17 @@
+n = int(input())
+price_lst = []
+
+for _ in range(n):
+    a,b,c = map(int, input().split())
+    if a == b and a == c and b == c:
+        s = 10000 + a * 1000
+    elif a != b and a != c and b != c:
+        s = max(a,b,c) * 100
+    else:
+        if a == b or a == c:
+            s = 1000 + a * 100
+        else:
+            s = 1000 + b * 100
+    price_lst.append(s)
+
+print(max(price_lst))
