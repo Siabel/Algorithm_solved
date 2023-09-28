c91fbafa17158bf8a7c99e0495d38b68ea14754f
Siabel <128214383+Siabel@users.noreply.github.com>
2023-09-29 05:01:18 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3248 ms, Memory: 243208 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+def find_set(x):
+    if parents[x] == x:
+        return x
+
+    parents[x] = find_set(parents[x])
+    return parents[x]
+
+
+def union(x, y):
+    x = find_set(x)
+    y = find_set(y)
+
+    if x == y:
+        return
+
+    if x < y:
+        parents[y] = x
+    else:
+        parents[x] = y
+
+n, m = map(int, input().split())
+parents = [0] * (n + 1)
+
+edge = []
+res = []
+
+parents = [i for i in range(n + 1)]
+
+for _ in range(m):
+    f, t, w = map(int, input().split())
+    edge.append([f, t, w])
+
+# w 를 기준으로 정렬
+edge.sort(key=lambda x: x[2])
+
+for f, t, w in edge:
+    if find_set(f) != find_set(t):
+        union(f, t)
+        res.append(w)
+
+print(sum(res) - max(res))
