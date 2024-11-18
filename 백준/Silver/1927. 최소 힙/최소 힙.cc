31850db949e57b270280834890bbae35096235e2
Siabel <128214383+Siabel@users.noreply.github.com>
2024-11-19 02:39:55 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  priority_queue<int, vector<int>, greater<int>> q;
+  
+  int n;
+  cin >> n;
+
+  for(int i = 0; i < n; i++){
+    int x;
+    cin >> x;
+    if(x == 0){
+      if(q.empty())
+        cout << '0';
+      else{
+        int a = q.top();
+        cout << a;
+        q.pop();        
+      }
+      cout << "\n";
+    }
+    else{
+      q.push(x);
+    }
+  }
+  return 0;
+}
