a16f6d9ec577ebbfd33dce8ced05edaf0c9bbc18
Siabel <128214383+Siabel@users.noreply.github.com>
2024-11-28 01:52:15 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 9
+using namespace std;
+
+int n, m;
+int arr[MAX];
+int save[MAX];
+
+void init(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> save[i];
+    }
+    sort(save, save + n);
+}
+
+void solve(int idx, int num){
+  if(idx == m){
+        for(int i = 0; i < m; i++){
+            cout << arr[i] << ' '; 
+        }
+        cout << "\n";
+        return;
+  }
+
+  int last = 0;
+  for(int i = num; i < n; i++){
+    if(save[i] != last){
+        arr[idx] = save[i];
+        last = save[i];
+        solve(idx + 1, i);
+    }
+  }
+}
+
+int main() {
+  init();
+  solve(0, 0);
+  return 0;
+}
