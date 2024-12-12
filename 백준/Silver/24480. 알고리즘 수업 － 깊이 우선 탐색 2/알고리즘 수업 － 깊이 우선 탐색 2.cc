5564230f0863feaa5b8f3de3052318a19e14d7ff
Siabel <128214383+Siabel@users.noreply.github.com>
2024-12-12 10:27:09 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 88 ms, Memory: 10932 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m, r;
+int cnt = 0;
+vector<int> visited;
+vector<vector<int>> graph;
+
+void init(){
+  ios_base::sync_with_stdio(false); 
+  cin.tie(0);
+
+  cin >> n >> m >> r;
+  graph.resize(n + 1);
+}
+
+void dfs(int start) {
+  if(visited[start]) {
+		return;
+	}
+
+  visited[start] = ++cnt;
+
+  for (int next : graph[start]) {
+    if (!visited[next]) {
+      dfs(next);
+    }
+  }
+}
+
+int main() {
+  init();
+  
+  for (int i = 0; i < m; ++i) {
+    int u, v;
+    cin >> u >> v;
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+  }
+
+  for (int i = 0; i < n + 1; ++i) {
+    sort(graph[i].begin(), graph[i].end(), greater<int>());
+  }
+
+  visited.resize(n + 1, 0);
+
+  dfs(r);
+
+  for(int i = 1; i <= n; i++){
+    cout << visited[i] << "\n";
+  }
+
+  return 0;
+}
